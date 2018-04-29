bff0aa2f776346bb8479917379daf5adf3f3946f
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2018-04-29 15:45:03 +0200

    Update add-on_storyboard.py

--- add-on_storyboard.py
+++ add-on_storyboard.py
@@ -52,7 +52,7 @@ def main_setup():
 
 
 def main_openFolder():
-    os.system('open ' + (bpy.path.abspath("//")))
+    os.system('open ' + (bpy.path.abspath('//')))
 
 
 def getShotNumberUnderPlayhead():
@@ -87,7 +87,7 @@ def stamp(s, f):
     bpy.context.scene.render.use_stamp_camera = False
     bpy.context.scene.render.use_stamp_filename = False
     bpy.context.scene.render.use_stamp_note = True
-    bpy.context.scene.render.stamp_note_text = "sh_" + "%03d" % (s + 1,) + " length: " + str(getShotLength(f))
+    bpy.context.scene.render.stamp_note_text = 'sh_' + '%03d' % (s + 1,) + ' length: ' + str(getShotLength(f))
 
 
 def main_exportImagesAll():
@@ -98,12 +98,12 @@ def main_exportImagesAll():
     for i in range(len(mrks) - 1):
         bpy.context.scene.frame_set(mrks[i])
         stamp(i, mrks[i])
-        bpy.context.scene.render.filepath = "//sh_" + "%03d" % (i + 1,)
+        bpy.context.scene.render.filepath = '//sh_' + '%03d' % (i + 1,)
         bpy.ops.render.opengl(write_still = True)
     #reset path and playhead
     bpy.context.scene.render.filepath = fp
     bpy.context.scene.frame_set(f)
-    bpy.context.scene.render.stamp_note_text = ""
+    bpy.context.scene.render.stamp_note_text = ''
     bpy.context.scene.render.use_stamp_note = False
     bpy.context.scene.render.use_stamp = False
     main_openFolder()
@@ -112,7 +112,7 @@ def main_exportImagesAll():
 def main_exportImagesIndividual():
     fp = bpy.context.scene.render.filepath
     f = bpy.context.scene.frame_current
-    bpy.context.scene.render.filepath = "//fr_" + "%03d" % (f,)
+    bpy.context.scene.render.filepath = '//fr_' + '%03d' % (f,)
     bpy.ops.render.opengl(write_still = True)
     bpy.context.scene.render.filepath = fp
     main_openFolder()
@@ -144,7 +144,7 @@ def main_exportAudioAll():
     for i in range(len(mrks) - 1):
         bpy.context.scene.frame_start = mrks[i]
         bpy.context.scene.frame_end = mrks[i + 1] - 1  
-        bpy.ops.sound.mixdown(filepath = bpy.path.abspath("//sh_") + "%03d" % (i + 1,) + ".wav", codec = 'PCM')
+        bpy.ops.sound.mixdown(filepath = bpy.path.abspath('//sh_') + '%03d' % (i + 1,) + '.wav', codec = 'PCM')
     #reset frame range
     bpy.context.scene.frame_start = rng_start
     bpy.context.scene.frame_end = rng_end
@@ -161,7 +161,7 @@ def main_exportAudioIndividual():
         if mrks[i] <= f < mrks[i + 1]:
             bpy.context.scene.frame_start = mrks[i]
             bpy.context.scene.frame_end = mrks[i + 1] - 1 
-            bpy.ops.sound.mixdown(filepath = bpy.path.abspath("//sh_") + "%03d" % (i + 1,) + ".wav", codec = 'PCM')
+            bpy.ops.sound.mixdown(filepath = bpy.path.abspath('//sh_') + '%03d' % (i + 1,) + '.wav', codec = 'PCM')
     #reset frame range
     bpy.context.scene.frame_start = rng_start
     bpy.context.scene.frame_end = rng_end
@@ -199,7 +199,7 @@ def checkStuffAudio():
 class Panel_setup(bpy.types.Panel):
     
     #panel attributes
-    """Info."""
+    '''Info.'''
     bl_label = 'Setup'
     bl_idname = 'tools_storyboard_setup'
     bl_space_type = 'VIEW_3D'
@@ -210,21 +210,15 @@ class Panel_setup(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         col = layout.column(align = True)
-        col.operator('script.operator_setup', text = "Setup Viewport")
-        col.operator('script.operator_open_folder', text = "Open Folder")
-        sh_amnt = len([marker.frame for marker in bpy.context.scene.timeline_markers]) - 1
-        if sh_amnt == -1:
-            sh_amnt = 0
-        layout.label('Total Shots: ' + str(sh_amnt))
-        layout.label('Shot: ' + str(getShotNumberUnderPlayhead()))
-        layout.label('Length: ' + str(getShotLength(bpy.context.scene.frame_current)))
-
+        col.operator('script.operator_setup', text = 'Setup Viewport')
+        col.operator('script.operator_open_folder', text = 'Open Folder')
+        
 
 #operator class
 class Operator_setup(bpy.types.Operator):
     
     #operator attributes
-    """Setup the viewport"""
+    '''Setup the viewport'''
     bl_label = 'Operator Setup'
     bl_idname = 'script.operator_setup'
     bl_options = {'REGISTER', 'UNDO'}
@@ -240,7 +234,7 @@ class Operator_setup(bpy.types.Operator):
 class Operator_openFolder(bpy.types.Operator):
     
     #operator attributes
-    """Open folder where the blend file is saved"""
+    '''Open folder where the blend file is saved'''
     bl_label = 'Operator Open Folder'
     bl_idname = 'script.operator_open_folder'
     bl_options = {'REGISTER', 'UNDO'}
@@ -265,7 +259,7 @@ class Operator_openFolder(bpy.types.Operator):
 class Panel_images(bpy.types.Panel):
     
     #panel attributes
-    """Images."""
+    '''Images.'''
     bl_label = 'Images'
     bl_idname = 'tools_storyboard_images'
     bl_space_type = 'VIEW_3D'
@@ -277,15 +271,15 @@ class Panel_images(bpy.types.Panel):
         layout = self.layout
         #layout.label('My Label')
         col = layout.column(align = True)
-        col.operator('script.operator_export_images_all', text = "Export All")
-        col.operator('script.operator_export_images_individual', text = "Export Current")
+        col.operator('script.operator_export_images_all', text = 'Export All')
+        col.operator('script.operator_export_images_individual', text = 'Export Current')
 
 
 #operator class
 class Operator_exportImagesAll(bpy.types.Operator):
     
     #operator attributes
-    """Export all images as png's cut at markers"""
+    '''Export all images as png's cut at markers'''
     bl_label = 'Operator Export Images All'
     bl_idname = 'script.operator_export_images_all'
     bl_options = {'REGISTER', 'UNDO'}
@@ -304,7 +298,7 @@ class Operator_exportImagesAll(bpy.types.Operator):
 class Operator_exportImagesIndividual(bpy.types.Operator):
     
     #operator attributes
-    """Export image under playhead as png"""
+    '''Export image under playhead as png'''
     bl_label = 'Operator Export Images Individual'
     bl_idname = 'script.operator_export_images_individual'
     bl_options = {'REGISTER', 'UNDO'}
@@ -329,7 +323,7 @@ class Operator_exportImagesIndividual(bpy.types.Operator):
 class Panel_audio(bpy.types.Panel):
     
     #panel attributes
-    """Audio."""
+    '''Audio.'''
     bl_label = 'Audio'
     bl_idname = 'tools_storyboard_audio'
     bl_space_type = 'VIEW_3D'
@@ -341,15 +335,15 @@ class Panel_audio(bpy.types.Panel):
         layout = self.layout
         #layout.label('My Label')
         col = layout.column(align = True)
-        col.operator('script.operator_export_audio_all', text = "Export All")
-        col.operator('script.operator_export_audio_individual', text = "Export Current")
+        col.operator('script.operator_export_audio_all', text = 'Export All')
+        col.operator('script.operator_export_audio_individual', text = 'Export Current')
 
 
 #operator class
 class Operator_exportAllAudio(bpy.types.Operator):
     
     #operator attributes
-    """Export all audio as clips cut at markers"""
+    '''Export all audio as clips cut at markers'''
     bl_label = 'Operator Export Audio All'
     bl_idname = 'script.operator_export_audio_all'
     bl_options = {'REGISTER', 'UNDO'}
@@ -369,7 +363,7 @@ class Operator_exportAllAudio(bpy.types.Operator):
 class Operator_exportAudioIndividual(bpy.types.Operator):
     
     #operator attributes
-    """Export audio clip under playhead cut by markers"""
+    '''Export audio clip under playhead cut by markers'''
     bl_label = 'Operator Export Audio Individual'
     bl_idname = 'script.operator_export_audio_individual'
     bl_options = {'REGISTER', 'UNDO'}
@@ -386,6 +380,33 @@ class Operator_exportAudioIndividual(bpy.types.Operator):
 
 
 #------------------------------------------------------------------------------------------------------------------------------
+# panel & operators - info
+#------------------------------------------------------------------------------------------------------------------------------
+
+#panel setup
+class Panel_info(bpy.types.Panel):
+    
+    #panel attributes
+    '''Info.'''
+    bl_label = 'Info'
+    bl_idname = 'tools_storyboard_info'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = 'Storyboard'
+    
+    #draw loop
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column(align = True)
+        sh_amnt = len([marker.frame for marker in bpy.context.scene.timeline_markers]) - 1
+        if sh_amnt == -1:
+            sh_amnt = 0
+        layout.label('Total Shots: ' + str(sh_amnt))
+        layout.label('Shot: ' + str(getShotNumberUnderPlayhead()))
+        layout.label('Length: ' + str(getShotLength(bpy.context.scene.frame_current)))
+
+
+#------------------------------------------------------------------------------------------------------------------------------
 # registration
 #------------------------------------------------------------------------------------------------------------------------------
 
@@ -394,6 +415,7 @@ def register():
     bpy.utils.register_class(Panel_setup)
     bpy.utils.register_class(Panel_images)
     bpy.utils.register_class(Panel_audio)
+    bpy.utils.register_class(Panel_info)
 
     bpy.utils.register_class(Operator_setup)
     bpy.utils.register_class(Operator_openFolder)
@@ -407,6 +429,7 @@ def unregister():
     bpy.utils.unregister_class(Panel_setup)
     bpy.utils.unregister_class(Panel_images)
     bpy.utils.unregister_class(Panel_audio)
+    bpy.utils.unregister_class(Panel_info)
 
     bpy.utils.unregister_class(Operator_setup)
     bpy.utils.unregister_class(Operator_openFolder)
